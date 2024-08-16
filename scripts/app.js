718ac3c52bc226743d3bcb919686fe05e7bf4546
bijay-1tamang <79555398+bijay-1tamang@users.noreply.github.com>
2024-08-16 16:08:08 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,54 @@
-// scripts/app.js
 new Vue({
     el: '#app',
     data: {
-      randomFact: '',
-      city: 'London Ontario',
-      newCity: '',
-      weather: {
-        temperature: '',
-        wind: '',
-        description: ''
-      },
-      word: {
+        randomFact: '',
+        weather: {
+            location: 'London, Ontario',
+            temperature: '',
+            wind: '',
+            description: ''
+        },
+        city: '',
         word: '',
-        phonetic: '',
-        partOfSpeech: '',
-        definition: ''
-      },
-      newWord: ''
+        definition: null
     },
     created() {
-      this.getRandomFact();
-      this.getWeather();
-      this.getDefinition('bottle');
+        this.getRandomFact();
+        this.getWeather();
     },
     methods: {
-      async getRandomFact() {
-        const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random');
-        const data = await response.json();
-        this.randomFact = data.text;
-      },
-      async getWeather() {
-        const city = this.newCity || this.city;
-        const response = await fetch(`https://goweather.herokuapp.com/weather/London%20Ontario`);
-        const data = await response.json();
-        this.weather.temperature = data.temperature;
-        this.weather.wind = data.wind;
-        this.weather.description = data.description;
-        this.city = city;
-        this.newCity = '';
-      },
-      async getDefinition(word) {
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/Bottle`);
-        const data = await response.json();
-        const definition = data[0].meanings[0].definitions[0];
-        this.word.word = data[0].word;
-        this.word.phonetic = data[0].phonetic;
-        this.word.partOfSpeech = data[0].meanings[0].partOfSpeech;
-        this.word.definition = definition.definition;
-        this.newWord = '';
-      }
+        getRandomFact() {
+            fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+                .then(response => response.json())
+                .then(data => {
+                    this.randomFact = data.text;
+                });
+        },
+        getWeather() {
+            const location = this.city || 'London Ontario';
+            fetch(`https://goweather.herokuapp.com/weather/London%20Ontario`)
+                .then(response => response.json())
+                .then(data => {
+                    this.weather = {
+                        location: location,
+                        temperature: data.temperature,
+                        wind: data.wind,
+                        description: data.description
+                    };
+                });
+        },
+        defineWord() {
+            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/Bottle`)
+                .then(response => response.json())
+                .then(data => {
+                    const entry = data[0];
+                    this.definition = {
+                        word: entry.word,
+                        phonetic: entry.phonetic,
+                        partOfSpeech: entry.meanings[0].partOfSpeech,
+                        meaning: entry.meanings[0].definitions[0].definition
+                    };
+                });
+        }
     }
-  });
+});
